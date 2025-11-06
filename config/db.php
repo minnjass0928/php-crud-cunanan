e361348dd3d3894e26a9402f7b7c699d2dd66ea9
jasmine cunanan <minnjass928@gmail.com>
2025-11-06 12:25:10 +0800

    added create feature with database connection

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root"; 
+$pass = ""; 
+$db_name = "branch_directory";
+
+$conn = mysqli_connect($host, $user, $pass, $db_name);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
