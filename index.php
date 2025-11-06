fd59dc8b13c0fd430409daf27bee8620183bd8c1
jasmine cunanan <minnjass928@gmail.com>
2025-11-06 13:15:55 +0800

    added homepage

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>student branch directory</title>
+</head>
+<body>
+
+<h2>student branch directory system</h2>
+
+<ul>
+    <li><a href="create.php">add student</a></li>
+    <li><a href="read.php">view students</a></li>
+</ul>
+
+</body>
+</html>
